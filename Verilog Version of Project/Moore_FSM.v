78771ab561f71bb158c6b3768117ab849d8694be
Ganojan <ganojan10@gmail.com>
2025-02-01 14:11:31 -0500

    Update Moore_FSM.v
    Optimized the code by reducing redundancy and ensuring first state when device is first turned on.

--- Verilog Version of Project/Moore_FSM.v
+++ Verilog Version of Project/Moore_FSM.v
@@ -1,11 +1,11 @@
 module Moore_FSM(
-	input data_in,
+	input data_in, //Declaring inputs and outputs of FSM
 	input clock,
 	input reset,
 	output reg [3:0] currentstate,
 	output reg [3:0] student_id
 );
-parameter  
+parameter  //Storing the 4-bit state values in easy-to-understand variables
 	s0 = 4'b0000, // s0 State
 	s1 = 4'b0001, // s1 State
 	s2 = 4'b0010, // s2 State
@@ -15,24 +15,23 @@ parameter
 	s6 = 4'b0110, // s6 State
 	s7 = 4'b0111; // s7 State
   
-reg [3:0] current_state, next_state;
+	reg [3:0] next_state = s0; //Used for determining the next state of  the FSM; initially set to s0 to ensure the first state is s0.
 
 always @(posedge clock, posedge reset) begin
  if(reset == 1) 
- current_state <= s0;
+ currentstate <= s0; //When reset is on, currentstate is set to s0
  else
- current_state <= next_state;
+ currentstate <= next_state; //If reset is off, currentstate is nextstate
 end 
 
-always @(current_state,data_in) begin
- case(current_state) 
+always @(currentstate,data_in) begin
+ case(currentstate)
  
- s0:begin
+ s0:begin //At currentstate s0, if data_in is 0, next state is s1, else next state is same state, s0. Corresponding tudent_id 'primary output' is attached the state.
 	if(data_in == 1)
 		next_state = s1;
 	else
 		next_state = s0;
-	currentstate = s0;
 	student_id = 4'b0000;
 	end
  
@@ -41,7 +40,6 @@ always @(current_state,data_in) begin
 		next_state = s2;
 	else
 		next_state = s1;
-	currentstate = s1;
 	student_id = 4'b0001;
 	end
 
@@ -50,7 +48,6 @@ always @(current_state,data_in) begin
 		next_state = s3;
 	else
 		next_state = s2;
-	currentstate = s2;
 	student_id = 4'b0010;
 	end
  
@@ -59,7 +56,6 @@ always @(current_state,data_in) begin
 		next_state = s4;
 	else
 		next_state = s3;
-	currentstate = s3;
 	student_id = 4'b0100;
 	end
  
@@ -68,7 +64,6 @@ always @(current_state,data_in) begin
 		next_state = s5;
 	else
 		next_state = s4;
-	currentstate = s4;
 	student_id = 4'b0010;
 	end
  s5:begin
@@ -76,7 +71,6 @@ always @(current_state,data_in) begin
 		next_state = s6;
 	else
 		next_state = s5;
-	currentstate = s5;
 	student_id = 4'b1000;
 	end
 
@@ -85,7 +79,6 @@ always @(current_state,data_in) begin
 		next_state = s7;
 	else
 		next_state = s6;
-	currentstate = s6;
 	student_id = 4'b0110;
 	end
 
@@ -94,7 +87,6 @@ always @(current_state,data_in) begin
 		next_state = s0;
 	else
 		next_state = s7;
-	currentstate = s7;
 	student_id = 4'b0101;
 	end
 
